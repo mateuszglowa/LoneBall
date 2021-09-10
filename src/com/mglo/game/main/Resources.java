9c483b2903dcf8e0a52f53b1bf3c0a7b8f8fcfc0
Mateusz Glowa <glowa.mateusz@gmail.com>
2021-09-10 13:53:27 +0200

    Added resources in resourcess class

--- src/com/mglo/game/main/Resources.java
+++ src/com/mglo/game/main/Resources.java
@@ -4,17 +4,25 @@ import javax.annotation.Resource;
 import javax.imageio.ImageIO;
 import java.applet.Applet;
 import java.applet.AudioClip;
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
 
 public class Resources {
 
-    public static BufferedImage welcome, iconimage;
+    public static BufferedImage welcome, iconimage, line;
+    public static AudioClip hit, bounce;
+    public static Color darkBlue, darkRed;
 
     public static void load(){
         welcome = loadImage("welcome.png");
         iconimage = loadImage("iconimage.png");
+        line = loadImage("line.png");
+        hit = loadSound("bounce.wav");
+        bounce = loadSound("hit.wav");
+        darkBlue = new Color(25, 83, 105);
+        darkRed = new Color(185, 13, 13);
     }
 
     public static AudioClip loadSound(String fileName){
