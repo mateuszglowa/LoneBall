d85718e3ac5654f12d643026f7815871252f67a0
Mateusz Glowa <glowa.mateusz@gmail.com>
2021-09-11 00:14:29 +0200

    added Ball class model - incomplete

---
+++ src/com/mglo/game/model/Ball.java
@@ -0,0 +1,58 @@
+package com.mglo.game.model;
+
+import com.mglo.framework.util.RandomNumberGenerator;
+import com.mglo.game.main.GameMain;
+import com.mglo.game.main.Resources;
+
+import java.awt.*;
+
+public class Ball {
+    private int x, y, width, height, velY, velX;
+    private Rectangle rect;
+
+    public Ball(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        velX = 5;
+        velY = RandomNumberGenerator.getRandIntBetween(-4, 5);
+        rect = new Rectangle(x, y, width, height);
+    }
+
+    public void update(){
+        x += velX;
+        y += velY;
+        correctYCollisions();
+        updateRect();
+    }
+
+    private void correctYCollisions(){
+        if(y < 0){
+            y = 0;
+        }else if (y + height > GameMain.GAME_HEIGHT){
+            y = GameMain.GAME_HEIGHT - height;
+        }else{
+            return;
+        }
+
+        velY = -velY;
+        Resources.bounce.play();
+    }
+
+    public void updateRect(){
+        rect.setBounds(x, y, width, height);
+    }
+
+    public void onCollideWith(Paddle p){
+            //TODO
+    }
+
+    public boolean isDead(){
+        //TODO
+    }
+
+    public void reset(){
+        //TODO
+    }
+}
